bba60da10d4a1c16e21e19adc0df0276c8bf453c
MohamedMounier <mohamedmounierabbas@gmail.com>
2025-05-07 02:39:31 +0300

    Initialzed Firebase Into Project

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+void main() async{
+  await dotenv.load(fileName: ".env");
+  WidgetsFlutterBinding.ensureInitialized();
 
-void main() {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 

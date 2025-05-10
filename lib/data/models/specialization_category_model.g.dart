fda50ebdee5d24fedea10f1239de3fdc1aa5c07e
MohamedMounier <mohamedmounierabbas@gmail.com>
2025-05-10 03:29:22 +0300

    Specialists Feature Logic
    -Added App Colors
    -Added Get Specialization categories
    -Added Get Specialists By Specialization categories
    -Added Choose Specialization Category

---
+++ lib/data/models/specialization_category_model.g.dart
@@ -0,0 +1,18 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'specialization_category_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+SpecializationCategoryModel _$SpecializationCategoryModelFromJson(
+  Map<String, dynamic> json,
+) => SpecializationCategoryModel(
+  id: json['id'] as String,
+  name: json['name'] as String,
+);
+
+Map<String, dynamic> _$SpecializationCategoryModelToJson(
+  SpecializationCategoryModel instance,
+) => <String, dynamic>{'id': instance.id, 'name': instance.name};

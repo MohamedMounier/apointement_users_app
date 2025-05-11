2a946321df943023fdaface7d01fc61baaff93a6
MohamedMounier <mohamedmounierabbas@gmail.com>
2025-05-11 21:17:25 +0300

    Added Edit User Appointment Feature
    -Added Cancel User Appointment
    -Added Reschedule User Appointment
    -Added Edit User Appointment Screen

---
+++ lib/presentation/blocs/appointments/edit_appointment_state.freezed.dart
@@ -0,0 +1,394 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'edit_appointment_state.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+  'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models',
+);
+
+/// @nodoc
+mixin _$EditAppointmentState {
+  EditAppointmentSteps get editAppointmentStep =>
+      throw _privateConstructorUsedError;
+  RequestState get requestState => throw _privateConstructorUsedError;
+  AppointmentStatus get filteredStatus => throw _privateConstructorUsedError;
+  String get errorMessage => throw _privateConstructorUsedError;
+  List<String>? get availableTimes => throw _privateConstructorUsedError;
+  String? get selectedTime => throw _privateConstructorUsedError;
+  DateTime? get selectedDate => throw _privateConstructorUsedError;
+  List<AppointmentActivityEntity> get currentAppointmentActivitiesList =>
+      throw _privateConstructorUsedError;
+  AppointmentEntity? get currentAppointment =>
+      throw _privateConstructorUsedError;
+
+  /// Create a copy of EditAppointmentState
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  $EditAppointmentStateCopyWith<EditAppointmentState> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $EditAppointmentStateCopyWith<$Res> {
+  factory $EditAppointmentStateCopyWith(
+    EditAppointmentState value,
+    $Res Function(EditAppointmentState) then,
+  ) = _$EditAppointmentStateCopyWithImpl<$Res, EditAppointmentState>;
+  @useResult
+  $Res call({
+    EditAppointmentSteps editAppointmentStep,
+    RequestState requestState,
+    AppointmentStatus filteredStatus,
+    String errorMessage,
+    List<String>? availableTimes,
+    String? selectedTime,
+    DateTime? selectedDate,
+    List<AppointmentActivityEntity> currentAppointmentActivitiesList,
+    AppointmentEntity? currentAppointment,
+  });
+}
+
+/// @nodoc
+class _$EditAppointmentStateCopyWithImpl<
+  $Res,
+  $Val extends EditAppointmentState
+>
+    implements $EditAppointmentStateCopyWith<$Res> {
+  _$EditAppointmentStateCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  /// Create a copy of EditAppointmentState
+  /// with the given fields replaced by the non-null parameter values.
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? editAppointmentStep = null,
+    Object? requestState = null,
+    Object? filteredStatus = null,
+    Object? errorMessage = null,
+    Object? availableTimes = freezed,
+    Object? selectedTime = freezed,
+    Object? selectedDate = freezed,
+    Object? currentAppointmentActivitiesList = null,
+    Object? currentAppointment = freezed,
+  }) {
+    return _then(
+      _value.copyWith(
+            editAppointmentStep:
+                null == editAppointmentStep
+                    ? _value.editAppointmentStep
+                    : editAppointmentStep // ignore: cast_nullable_to_non_nullable
+                        as EditAppointmentSteps,
+            requestState:
+                null == requestState
+                    ? _value.requestState
+                    : requestState // ignore: cast_nullable_to_non_nullable
+                        as RequestState,
+            filteredStatus:
+                null == filteredStatus
+                    ? _value.filteredStatus
+                    : filteredStatus // ignore: cast_nullable_to_non_nullable
+                        as AppointmentStatus,
+            errorMessage:
+                null == errorMessage
+                    ? _value.errorMessage
+                    : errorMessage // ignore: cast_nullable_to_non_nullable
+                        as String,
+            availableTimes:
+                freezed == availableTimes
+                    ? _value.availableTimes
+                    : availableTimes // ignore: cast_nullable_to_non_nullable
+                        as List<String>?,
+            selectedTime:
+                freezed == selectedTime
+                    ? _value.selectedTime
+                    : selectedTime // ignore: cast_nullable_to_non_nullable
+                        as String?,
+            selectedDate:
+                freezed == selectedDate
+                    ? _value.selectedDate
+                    : selectedDate // ignore: cast_nullable_to_non_nullable
+                        as DateTime?,
+            currentAppointmentActivitiesList:
+                null == currentAppointmentActivitiesList
+                    ? _value.currentAppointmentActivitiesList
+                    : currentAppointmentActivitiesList // ignore: cast_nullable_to_non_nullable
+                        as List<AppointmentActivityEntity>,
+            currentAppointment:
+                freezed == currentAppointment
+                    ? _value.currentAppointment
+                    : currentAppointment // ignore: cast_nullable_to_non_nullable
+                        as AppointmentEntity?,
+          )
+          as $Val,
+    );
+  }
+}
+
+/// @nodoc
+abstract class _$$EditAppointmentStateImplCopyWith<$Res>
+    implements $EditAppointmentStateCopyWith<$Res> {
+  factory _$$EditAppointmentStateImplCopyWith(
+    _$EditAppointmentStateImpl value,
+    $Res Function(_$EditAppointmentStateImpl) then,
+  ) = __$$EditAppointmentStateImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call({
+    EditAppointmentSteps editAppointmentStep,
+    RequestState requestState,
+    AppointmentStatus filteredStatus,
+    String errorMessage,
+    List<String>? availableTimes,
+    String? selectedTime,
+    DateTime? selectedDate,
+    List<AppointmentActivityEntity> currentAppointmentActivitiesList,
+    AppointmentEntity? currentAppointment,
+  });
+}
+
+/// @nodoc
+class __$$EditAppointmentStateImplCopyWithImpl<$Res>
+    extends _$EditAppointmentStateCopyWithImpl<$Res, _$EditAppointmentStateImpl>
+    implements _$$EditAppointmentStateImplCopyWith<$Res> {
+  __$$EditAppointmentStateImplCopyWithImpl(
+    _$EditAppointmentStateImpl _value,
+    $Res Function(_$EditAppointmentStateImpl) _then,
+  ) : super(_value, _then);
+
+  /// Create a copy of EditAppointmentState
+  /// with the given fields replaced by the non-null parameter values.
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? editAppointmentStep = null,
+    Object? requestState = null,
+    Object? filteredStatus = null,
+    Object? errorMessage = null,
+    Object? availableTimes = freezed,
+    Object? selectedTime = freezed,
+    Object? selectedDate = freezed,
+    Object? currentAppointmentActivitiesList = null,
+    Object? currentAppointment = freezed,
+  }) {
+    return _then(
+      _$EditAppointmentStateImpl(
+        editAppointmentStep:
+            null == editAppointmentStep
+                ? _value.editAppointmentStep
+                : editAppointmentStep // ignore: cast_nullable_to_non_nullable
+                    as EditAppointmentSteps,
+        requestState:
+            null == requestState
+                ? _value.requestState
+                : requestState // ignore: cast_nullable_to_non_nullable
+                    as RequestState,
+        filteredStatus:
+            null == filteredStatus
+                ? _value.filteredStatus
+                : filteredStatus // ignore: cast_nullable_to_non_nullable
+                    as AppointmentStatus,
+        errorMessage:
+            null == errorMessage
+                ? _value.errorMessage
+                : errorMessage // ignore: cast_nullable_to_non_nullable
+                    as String,
+        availableTimes:
+            freezed == availableTimes
+                ? _value._availableTimes
+                : availableTimes // ignore: cast_nullable_to_non_nullable
+                    as List<String>?,
+        selectedTime:
+            freezed == selectedTime
+                ? _value.selectedTime
+                : selectedTime // ignore: cast_nullable_to_non_nullable
+                    as String?,
+        selectedDate:
+            freezed == selectedDate
+                ? _value.selectedDate
+                : selectedDate // ignore: cast_nullable_to_non_nullable
+                    as DateTime?,
+        currentAppointmentActivitiesList:
+            null == currentAppointmentActivitiesList
+                ? _value._currentAppointmentActivitiesList
+                : currentAppointmentActivitiesList // ignore: cast_nullable_to_non_nullable
+                    as List<AppointmentActivityEntity>,
+        currentAppointment:
+            freezed == currentAppointment
+                ? _value.currentAppointment
+                : currentAppointment // ignore: cast_nullable_to_non_nullable
+                    as AppointmentEntity?,
+      ),
+    );
+  }
+}
+
+/// @nodoc
+
+class _$EditAppointmentStateImpl implements _EditAppointmentState {
+  const _$EditAppointmentStateImpl({
+    this.editAppointmentStep = EditAppointmentSteps.none,
+    this.requestState = RequestState.none,
+    this.filteredStatus = AppointmentStatus.confirmed,
+    this.errorMessage = '',
+    final List<String>? availableTimes,
+    this.selectedTime,
+    this.selectedDate,
+    final List<AppointmentActivityEntity> currentAppointmentActivitiesList =
+        const [],
+    this.currentAppointment,
+  }) : _availableTimes = availableTimes,
+       _currentAppointmentActivitiesList = currentAppointmentActivitiesList;
+
+  @override
+  @JsonKey()
+  final EditAppointmentSteps editAppointmentStep;
+  @override
+  @JsonKey()
+  final RequestState requestState;
+  @override
+  @JsonKey()
+  final AppointmentStatus filteredStatus;
+  @override
+  @JsonKey()
+  final String errorMessage;
+  final List<String>? _availableTimes;
+  @override
+  List<String>? get availableTimes {
+    final value = _availableTimes;
+    if (value == null) return null;
+    if (_availableTimes is EqualUnmodifiableListView) return _availableTimes;
+    // ignore: implicit_dynamic_type
+    return EqualUnmodifiableListView(value);
+  }
+
+  @override
+  final String? selectedTime;
+  @override
+  final DateTime? selectedDate;
+  final List<AppointmentActivityEntity> _currentAppointmentActivitiesList;
+  @override
+  @JsonKey()
+  List<AppointmentActivityEntity> get currentAppointmentActivitiesList {
+    if (_currentAppointmentActivitiesList is EqualUnmodifiableListView)
+      return _currentAppointmentActivitiesList;
+    // ignore: implicit_dynamic_type
+    return EqualUnmodifiableListView(_currentAppointmentActivitiesList);
+  }
+
+  @override
+  final AppointmentEntity? currentAppointment;
+
+  @override
+  String toString() {
+    return 'EditAppointmentState(editAppointmentStep: $editAppointmentStep, requestState: $requestState, filteredStatus: $filteredStatus, errorMessage: $errorMessage, availableTimes: $availableTimes, selectedTime: $selectedTime, selectedDate: $selectedDate, currentAppointmentActivitiesList: $currentAppointmentActivitiesList, currentAppointment: $currentAppointment)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$EditAppointmentStateImpl &&
+            (identical(other.editAppointmentStep, editAppointmentStep) ||
+                other.editAppointmentStep == editAppointmentStep) &&
+            (identical(other.requestState, requestState) ||
+                other.requestState == requestState) &&
+            (identical(other.filteredStatus, filteredStatus) ||
+                other.filteredStatus == filteredStatus) &&
+            (identical(other.errorMessage, errorMessage) ||
+                other.errorMessage == errorMessage) &&
+            const DeepCollectionEquality().equals(
+              other._availableTimes,
+              _availableTimes,
+            ) &&
+            (identical(other.selectedTime, selectedTime) ||
+                other.selectedTime == selectedTime) &&
+            (identical(other.selectedDate, selectedDate) ||
+                other.selectedDate == selectedDate) &&
+            const DeepCollectionEquality().equals(
+              other._currentAppointmentActivitiesList,
+              _currentAppointmentActivitiesList,
+            ) &&
+            (identical(other.currentAppointment, currentAppointment) ||
+                other.currentAppointment == currentAppointment));
+  }
+
+  @override
+  int get hashCode => Object.hash(
+    runtimeType,
+    editAppointmentStep,
+    requestState,
+    filteredStatus,
+    errorMessage,
+    const DeepCollectionEquality().hash(_availableTimes),
+    selectedTime,
+    selectedDate,
+    const DeepCollectionEquality().hash(_currentAppointmentActivitiesList),
+    currentAppointment,
+  );
+
+  /// Create a copy of EditAppointmentState
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$EditAppointmentStateImplCopyWith<_$EditAppointmentStateImpl>
+  get copyWith =>
+      __$$EditAppointmentStateImplCopyWithImpl<_$EditAppointmentStateImpl>(
+        this,
+        _$identity,
+      );
+}
+
+abstract class _EditAppointmentState implements EditAppointmentState {
+  const factory _EditAppointmentState({
+    final EditAppointmentSteps editAppointmentStep,
+    final RequestState requestState,
+    final AppointmentStatus filteredStatus,
+    final String errorMessage,
+    final List<String>? availableTimes,
+    final String? selectedTime,
+    final DateTime? selectedDate,
+    final List<AppointmentActivityEntity> currentAppointmentActivitiesList,
+    final AppointmentEntity? currentAppointment,
+  }) = _$EditAppointmentStateImpl;
+
+  @override
+  EditAppointmentSteps get editAppointmentStep;
+  @override
+  RequestState get requestState;
+  @override
+  AppointmentStatus get filteredStatus;
+  @override
+  String get errorMessage;
+  @override
+  List<String>? get availableTimes;
+  @override
+  String? get selectedTime;
+  @override
+  DateTime? get selectedDate;
+  @override
+  List<AppointmentActivityEntity> get currentAppointmentActivitiesList;
+  @override
+  AppointmentEntity? get currentAppointment;
+
+  /// Create a copy of EditAppointmentState
+  /// with the given fields replaced by the non-null parameter values.
+  @override
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  _$$EditAppointmentStateImplCopyWith<_$EditAppointmentStateImpl>
+  get copyWith => throw _privateConstructorUsedError;
+}
